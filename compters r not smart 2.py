ff6ea2183a0e87e4d1a13135528bcc509de42a58
drewdoescode <drewdoesgamemaker@gmail.com>
2014-07-07 20:09:51 -0400

    first version

---
+++ compters r not smart 2.py
@@ -0,0 +1,20 @@
+print ("hello world")
+print ("is pizza a living thing")
+print ("PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE")
+name =input ("is pie a living thing?")
+print ("yes pie is "+ name + ". a living thing")
+
+answer = input(" are you smart computer?")
+
+if answer == "yes":
+    print (" oh you are a rainbow of poptart cats ? " + answer)
+elif answer == "no":
+    print ("i knew that you arent gonna be smart till i teach you everything!")
+           
+       
+       
+
+
+
+
+       
---
+++ computers r not smart.py
@@ -0,0 +1,147 @@
+Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 122122122122122122122122122/12231223122312231223
+9984457.065405553
+>>> print("hello world")
+hello world
+>>> print ("apple pie")
+apple pie
+>>> input ("do you like pie?")
+do you like pie?no pie is terrible!
+'no pie is terrible!'
+>>> input ("you are in front of a lovly double rainbow what do yoiu do")
+you are in front of a lovly double rainbow what do yoiu doyou eat the rainbow and leave the other one
+'you eat the rainbow and leave the other one'
+>>> ================================ RESTART ================================
+>>> 
+hello world
+hello world
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+Traceback (most recent call last):
+  File "C:/Python34/hello world.py", line 4, in <module>
+    impit ("is pie a living thing ?")
+NameError: name 'impit' is not defined
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+Traceback (most recent call last):
+  File "C:/Python34/hello world.py", line 4, in <module>
+    imput ("is pie a living thing ?")
+NameError: name 'imput' is not defined
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+Traceback (most recent call last):
+  File "C:/Python34/hello world.py", line 4, in <module>
+    imput ("is pie a living thing?")
+NameError: name 'imput' is not defined
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+is pie a living thing?
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+is pie a living thing?enter
+yes pie is enter. a living thing
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+is pie a living thing?yes
+yes pie is yes. a living thing
+Traceback (most recent call last):
+  File "C:/Python34/hello world.py", line 6, in <module>
+    if answer == " are you smart computer":
+NameError: name 'answer' is not defined
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+is pie a living thing?yes
+yes pie is yes. a living thing
+ are you smart computer?no
+i knew that.
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+is pie a living thing?no
+yes pie is no. a living thing
+ are you smart computer?yes
+ oh you yes
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+is pie a living thing?yes
+yes pie is yes. a living thing
+ are you smart computer?yes
+ oh you are ? yes
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+is pie a living thing?yes
+yes pie is yes. a living thing
+ are you smart computer?no
+i knew that.
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+is pie a living thing?yes
+yes pie is yes. a living thing
+ are you smart computer?no
+i knew that you arent gonna be smart till i teach you everything!
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+is pie a living thing?yes
+yes pie is yes. a living thing
+ are you smart computer?yes
+ oh you are a rainbow of poptart cats ? yes
+>>> ================================ RESTART ================================
+>>> 
+hello world
+is pizza a living thing
+PIE PIE PIE PIE PIE PIE PIE PIE PIE PIE
+is pie a living thing?no no no no no no no no no no no
+yes pie is no no no no no no no no no no no. a living thing
+ are you smart computer?yes
+ oh you are a rainbow of poptart cats ? yes
+>>> 
