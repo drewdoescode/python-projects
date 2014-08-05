cdad4cbb975095ba26816c72389c165f145663e3
drewdoescode <drewdoesgamemaker@gmail.com>
2014-08-04 21:07:51 -0400

    i started to make a quiz that i will finish tommorrow or next week
    apple pie

---
+++ drews python.py
@@ -0,0 +1,18 @@
+answer = input("hello ready to be quized")
+if answer.startswith("yes"):
+    print("alright lets let you be quized")
+answer = input("how do you craft a piston in minecraft")
+if answer.startswith("i have no clue"):
+    print("oh yeah you have to have a crafting table to tell me dugh sorry bro")
+
+answer = input("whats 2*2")
+if answer.startswith("4"):
+    print("yes you are right cangrats")
+else:
+    print("oh man you got it wrong try one more time if you get it wrong this time then we will move on! sorry")
+
+answer = input("whats 2*2")
+if answer.startswith("4"):
+    print("yes you are right congrats")
+else:
+    print("sorry you got it wrong again lets move on you still have two lives left oh yeah you only have three lives to live so yeah")
