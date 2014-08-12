ba51e7f1082d3c20bbf9621e8477f2d6ddae75e3
drewdoescode <drewdoesgamemaker@gmail.com>
2014-08-11 20:01:26 -0400

    i started to make a monty python thing

---
+++ fun.py
@@ -0,0 +1,12 @@
+print('''who would cross the bridge of death must answer me thess
+quetions three, 'ere the othermside he see ...
+
+
+''')
+
+
+
+answer = input("what is your name?")
+if answer.startswith("lancelot"):
+    print("ok")
+answer = input
