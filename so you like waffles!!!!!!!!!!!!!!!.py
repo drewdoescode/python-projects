2cfb0b17fb4fd9f8ec1dd559a2343a3b3f5daf3f
drewdoescode <drewdoesgamemaker@gmail.com>
2014-08-18 20:35:47 -0400

    do you like waffles!?
    apple pie

---
+++ so you like waffles!!!!!!!!!!!!!!!.py
@@ -0,0 +1,17 @@
+answer = input("do you like waffles")
+if answer.startswith("yes"):
+    print("yeah we like waffles!")
+else:
+    print("we DO")
+answer = input("do you like pancakes")
+if answer.startswith("yes"):
+    print("yeah we like pancakes")
+else:
+    print("we DO")
+answer = input("do you like frenchtoast")
+if answer.startswith("yes"):
+    print("do do do do cant wait to get a mouthful")
+else:
+    print("ok bye FREIND :D")
+while True:
+    print("apple p
