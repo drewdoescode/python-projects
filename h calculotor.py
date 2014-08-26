cadcf235072c69138bbc732854c9c040276fc14a
drewdoescode <drewdoesgamemaker@gmail.com>
2014-08-25 20:26:56 -0400

    age calculator
    apple pie

---
+++ h calculotor.py
@@ -0,0 +1,19 @@
+print("Welcome to the age calculator")
+print("What is your name?")
+name1 = input()
+print(name1)
+answer1 = ("drew")
+answer2 = ("gabe")
+name2 = input("what is your little friends name?")
+print(name2)
+number1 = input("how old is " + answer1)
+number2 = input("how old is" + answer2)
+number1 = int(number1)
+number2 = int(number2)
+age1 = (11)
+age2 = (11)
+years_apart = age1 + age2
+years_apart = abs(years_apart)
+print(years_apart)
+
+
