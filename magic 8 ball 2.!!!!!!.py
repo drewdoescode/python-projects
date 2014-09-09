0016a682d2a2016b324453bf4dacf22a678ca7df
drewdoescode <drewdoesgamemaker@gmail.com>
2014-09-08 20:04:35 -0400

    this one is awesome

---
+++ magic 8 ball 2.!!!!!!.py
@@ -0,0 +1,24 @@
+import random
+print("welome to the magis 8 ball")
+print("ask me a quistion")
+while True:
+    answers = ['yes','no','definitly','maybe','kinda',
+    'APPLE PIE','yes and you also smell like tacos',
+    'STOP COPYING ME!']
+
+    quistion = input("> '")
+
+
+
+
+
+
+
+
+
+
+
+
+
+    answer = random.choice(answers)
+    print(answer)
