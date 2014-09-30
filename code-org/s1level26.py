e40af5f3fb8edaf4da3e49817ec18ec2f58f3465
drewdoescode <drewdoesgamemaker@gmail.com>
2014-09-29 20:44:14 -0400

    pretty cool java script to python!

---
+++ code-org/s1level26.py
@@ -0,0 +1,18 @@
+#http://studio.code.org/s/1/level/26
+'''
+for (var count = 0; count < 4; count++) {
+  moveForward(100);
+  turnRight(90);
+}
+'''
+
+
+
+
+
+import turtle
+bob = turtle.Turtle()
+bob.pensize(7)
+for count in range(4):
+     bob.forward(100)
+     bob.right(90)
